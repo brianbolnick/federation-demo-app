0d46e14170aff393bfcfc1e0e4c1c90ac3279de0
Brian Bolnick <brianbolnick@gmail.com>
2021-04-18 11:36:08 -0600

    update

--- src/About.js
+++ src/About.js
@@ -3,8 +3,8 @@ import React, { lazy } from 'react';
 const Button = lazy(() => import('federation_demo_design/Button'));
 
 const AboutPage = () => (
-  <div>
-    <h1>About Page</h1>
+  <div style={{ padding: '16px' }}>
+    <h1 style={{ color: '#264653' }}>About Page</h1>
 
     <React.Suspense fallback="Loading Button...">
       <Button>Click me on about!</Button>
